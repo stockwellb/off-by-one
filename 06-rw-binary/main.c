47d717b1e0a26bf6576d3fba43565782bc4c4651
Bruce Stockwell <bruce.stockwell@gmail.com>
2026-07-23 22:01:54 -0400

    code improvement

--- 06-rw-binary/main.c
+++ 06-rw-binary/main.c
@@ -8,44 +8,45 @@ typedef struct {
   size_t y;
 } point;
 
-int write_file(void) {
+int write_file(const char *path, const point *out) {
 
-  FILE *file = fopen("test.bin", "wb");
+  FILE *file = fopen(path, "wb");
 
   if (file == NULL) {
     perror("fopen");
     return EXIT_FAILURE;
   }
 
-  point p = {.x = 100, .y = 125};
-
-  size_t num = fwrite(&p, sizeof(p), 1, file);
+  size_t num = fwrite(out, sizeof *out, 1, file);
 
   if (num != 1) {
-    perror("fwite");
+    perror("fwrite");
+    fclose(file);
     return EXIT_FAILURE;
   }
 
-  fclose(file);
+  if (fclose(file) != 0) {
+    perror("fclose");
+    return EXIT_FAILURE;
+  }
+
   return EXIT_SUCCESS;
 }
 
-int read_file(void) {
-  FILE *file = fopen("test.bin", "rb");
+int read_file(const char *path, point *out) {
+  FILE *file = fopen(path, "rb");
 
   if (file == NULL) {
     perror("fopen");
     return EXIT_FAILURE;
   }
 
-  point p;
-
-  size_t num = fread(&p, sizeof(p), 1, file);
+  size_t num = fread(out, sizeof *out, 1, file);
 
   if (num != 1) {
     if (feof(file)) {
-      fprintf(stderr, "enexpected end of file\n");
-    } else {
+      fprintf(stderr, "unexpected end of file\n");
+    } else if (ferror(file)) {
       perror("fread");
     }
     fclose(file);
@@ -53,15 +54,25 @@ int read_file(void) {
   }
 
   fclose(file);
-  printf("x: %zu\ny: %zu\n", p.x, p.y);
 
   return EXIT_SUCCESS;
 }
 
 int main(void) {
 
-  write_file();
-  read_file();
+  const char *path = "test.bin";
+
+  point p1 = {.x = 100, .y = 125};
+  if (write_file(path, &p1) != EXIT_SUCCESS) {
+    return EXIT_FAILURE;
+  }
+
+  point p2;
+  if (read_file(path, &p2) != EXIT_SUCCESS) {
+    return EXIT_FAILURE;
+  }
+
+  printf("x: %zu\ny: %zu\n", p2.x, p2.y);
 
   return EXIT_SUCCESS;
 }
